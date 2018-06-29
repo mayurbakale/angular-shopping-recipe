8c7968934d37a928822ae98d28176ab24fa0cff8
Mayur Bakale <mayurbakale@gmail.com>
2018-06-29 16:57:43 +0530

    Shopping Recipe Third Commit

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,13 +7,20 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
   recipes: Recipe[] = [
-    new Recipe('A Test Recipe','This is simply a Test Recipe','http://knoxpa.com/wp-content/uploads/2017/12/recipes-header-image.jpg')
+    new Recipe('A Test Recipe','This is simply a Test Recipe','http://knoxpa.com/wp-content/uploads/2017/12/recipes-header-image.jpg'),
+    new Recipe('A Test Recipe','This is simply a Test Recipe 2','http://knoxpa.com/wp-content/uploads/2017/12/recipes-header-image.jpg')
   ];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRecipeSelected(recipe: Recipe){
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, EventEmitter, Output } from '@angular/core';
+import { Ingredient } from '../../shared/Ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppingEditComponent implements OnInit {
 
+  @ViewChild('nameInput') nameInput: ElementRef;
+  @ViewChild('amountInput') amountInput: ElementRef;
+
+  @Output() ingredientEmitter = new EventEmitter<Ingredient>();
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onAddItem() {
+    const ingName = this.nameInput.nativeElement.value
+    const ingAmount = this.amountInput.nativeElement.value
+    const newIngredient = new Ingredient(ingName,ingAmount)
+    this.ingredientEmitter.emit(newIngredient)
   }
 
 }
